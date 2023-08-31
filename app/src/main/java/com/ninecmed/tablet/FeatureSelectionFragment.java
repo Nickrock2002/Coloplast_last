275a1e81964bd683285f5357db75a1bcd36357cf
mayur_patil <mayur.patil@reposenergy.com>
2023-08-31 17:09:06 +0530

    sequencing of dialogs

--- app/src/main/java/com/ninecmed/tablet/FeatureSelectionFragment.java
+++ app/src/main/java/com/ninecmed/tablet/FeatureSelectionFragment.java
@@ -1,9 +1,12 @@
 package com.ninecmed.tablet;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,8 +29,86 @@ public class FeatureSelectionFragment extends Fragment {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //((MainActivity)getActivity()).showResetCounterDialog();
+                //showSetDateTimeDialog();
             }
         });
+    }
+
+    //TODO- call this method once the bluetooth dialog setup flow is done
+    public void showSetDateTimeDialog() {
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_set_date_time);
+
+        Button btnDate = (Button) dialog.findViewById(R.id.btn_date);
+        Button btnTime = (Button) dialog.findViewById(R.id.btn_time);
+        Button btnCancel = (Button) dialog.findViewById(R.id.btn_cancel);
+        Button btnConfirm = (Button) dialog.findViewById(R.id.btn_confirm);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+                dialog.dismiss();
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+                dialog.dismiss();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: save delta time
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void showTimePickerDialog() {
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_time_picker);
+
+        Button btnConfirmTime = (Button) dialog.findViewById(R.id.btn_confirm_time);
+        btnConfirmTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSetDateTimeDialog();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void showDatePickerDialog() {
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_date_picker);
+
+        Button btnConfirmDate = (Button) dialog.findViewById(R.id.btn_confirm_date);
+        btnConfirmDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSetDateTimeDialog();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
