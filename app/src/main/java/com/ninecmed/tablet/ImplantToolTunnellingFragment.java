09c76a23e8583325874d2b45218ea2917fcde723
Nikhil Patil <nikhil.patil@joshsoftware.com>
2023-12-04 20:27:39 +0530

    Minor bug fix

--- app/src/main/java/com/ninecmed/tablet/ImplantToolTunnellingFragment.java
+++ app/src/main/java/com/ninecmed/tablet/ImplantToolTunnellingFragment.java
@@ -187,6 +187,7 @@ public class ImplantToolTunnellingFragment extends Fragment {
     @SuppressLint("DefaultLocale")
     public void updateImplantTunnellingUI(boolean success) {
         if (mMainActivity.wandComm.getCurrentJob() == WandComm.jobs.SETSTIMEXT) {
+            mMainActivity.isInterrogationDone = true;
             binding.btExternalStartStim.setEnabled(true);
             binding.btExternalStartStim.setPressed(false);
             binding.btExternalStartStim.setText(R.string.hold_to_deliver_neurostimulation);
